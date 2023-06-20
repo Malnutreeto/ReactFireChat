b4b6cbaa4a784dc91707593fda541ffedfa02da5
Emanuele Tritone <129194906+Malnutreeto@users.noreply.github.com>
2023-06-20 09:55:07 +0200

    app.css deleted for grid system

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Cookies from 'universal-cookie';
 import Chat from './components/Chat';
 import { signOut } from 'firebase/auth';
 import { auth } from './firebase-config';
-import './styles/App.css';
+import { Grid } from '@mui/material';
 
 const cookies = new Cookies();
 
@@ -18,7 +18,7 @@ function App() {
   const [isAuth, setIsAuth] = useState(cookies.get('auth-token'));
   const [room, setRoom] = useState(null);
 
-  
+
 
   //per evitare che con un banale onChange sull' <input /> ci restituisca un true (e quindi restituendoci la chat senza aver inserito il nome della chatroom
   //dobbiamo settare un useRef per prendere esattamente l'elemento che ci serve "modificare"
@@ -46,28 +46,78 @@ function App() {
       {room ? (
         <Chat room={room} />
       ) : (
-        <div>
-          <form 
-          
-          className='room'>
-            <label>Enter Room Name: </label>
-            <input ref={roomInputRef} />
+        <Grid>
+          <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100vw",
+            height: "35vh",
+            fontFamily:"'Segoe UI', Tahoma, Geneva, Verdana, sans-serif"
+          }}
+          >
+            <label
+              style={{
+                textAlign: "center",
+                fontSize: "25px",
+                marginBottom: "20px"
+              }}
+            >Enter Room Name: </label>
+            <input
+              style={{
+                width: "200px",
+                height: "40px",
+                border: "2px solid #4c983b",
+                borderRadius: "6px",
+                paddingLeft: "5px",
+                fontSize: "20px",
+                textAlign: "center",
+                margin: "5px"
+              }}
+              ref={roomInputRef} />
             {/* imposto il ritorno della chatroom col value corrente dell'elemento selezionato con useRef SOLO dopo che clicco sul bottone*/}
             <button
-              className='room-button'
+              style={{
+                width: "210px",
+                height: "40px",
+                border: "none",
+                borderRadius: "6px",
+                paddingLeft: "5px",
+                fontSize: "20px",
+                texAlign: "center",
+                margin: "5px",
+                backgroundColor: "#4c983b",
+                color: "white",
+                cursor: "pointer"
+              }}
               onClick={() => setRoom(roomInputRef.current.value)}
               type='submit'>
               Enter Chat!
             </button>
           </form>
-          <div className='sign-out-area'>
+          <Grid
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
             <button
-              className='sign-out'
+              style={{
+                cursor: "pointer",
+                fontSize: "20px",
+                background: "transparent",
+                color: "#ffffff",
+                borderRadius: "6px",
+                border: "2px solid #4c983b",
+                padding: "10px",
+                width: "fit-content"
+              }}
               onClick={signUserOut}>
               Sign Out
             </button>
-          </div>
-        </div>
+          </Grid>
+        </Grid >
       )
       }
     </>)
